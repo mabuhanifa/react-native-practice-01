72466b3da2583a6b64627b6ce7086dc4a3e8205f
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-26 16:34:36 +0600

    filtered input added

--- components/Countries.js
+++ components/Countries.js
@@ -1,23 +1,49 @@
 import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import Country from "./Country";
 
 export default function countries() {
   const [countries, setCountries] = useState([]);
+
+  const [search, setSearch] = useState([]);
+
   useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setCountries(data))
-    
-  },[])
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setSearch(data);
+        setCountries(data);
+      });
+  }, []);
+  const handleSearch = (text) => {
+    const filtered = countries.filter((country) =>
+      country.name.common.includes(text)
+    );
+    setSearch(filtered);
+  };
   return (
     <View>
       <Text>Countries : {countries.length} </Text>
+      <TextInput 
+      onChangeText={handleSearch}
+      style={styles.input}
+      >
+
+      </TextInput>
       <ScrollView>
-{
-    countries.map((country) => <Country country={country}></Country>)
-}
+        {search.map((country) => (
+          <Country country={country}></Country>
+        ))}
       </ScrollView>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  input: {
+    marginTop: 50,
+    borderBottomColor: "#000000",
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 5,
+  },
+});
--- components/Country.js
+++ components/Country.js
@@ -1,9 +1,24 @@
-import { Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from "react-native";
 
-export default function Country({country}) {
+export default function Country({ country }) {
   return (
     <View>
-      <Text>Country {country.name.common}</Text>
+      <Text style={styles.header}>Country : {country.name.common}</Text>
+      <Image
+        source={{
+          uri: country.flags.png,
+        }}
+        style={{ width: 200, height: 200 }}
+      ></Image>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+    header :{
+        marginTop: 10,
+        marginBottom: 10,
+        fontSize: 20,
+        color : "#2e2e2e",
+    }
+});
