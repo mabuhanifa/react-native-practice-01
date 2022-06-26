fbfa588be4f037d900410c53bab052b1bc14091e
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-26 15:53:24 +0600

    country added

--- components/Countries.js
+++ components/Countries.js
@@ -1,9 +1,23 @@
-import { Text, View } from 'react-native'
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import Country from "./Country";
 
 export default function countries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+    
+  },[])
   return (
     <View>
-      <Text>Countries : </Text>
+      <Text>Countries : {countries.length} </Text>
+      <ScrollView>
+{
+    countries.map((country) => <Country country={country}></Country>)
+}
+      </ScrollView>
     </View>
-  )
+  );
 }
